bf1c719194cc0c59ed524d453a7ca360aa6bba5a
solar05 <artem00298@gmail.com>
2021-06-14 23:14:52 +0300

    Add one more test

--- spec/dockcheck_spec.rb
+++ spec/dockcheck_spec.rb
@@ -1,3 +1,13 @@
 require 'spec_helper'
 
-RSpec.describe DockCheck do; end
+RSpec.describe DockCheck do
+  let(:checker) { DockCheck.new(:snils) }
+
+  it 'check and swaps correctly' do
+    expect(checker.check("12345678911")).to be false
+
+    checker.change_checker(:snils)
+
+    expect(checker.check("12345434343")).to be false
+  end
+end
