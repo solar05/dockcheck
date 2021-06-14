81831e47d6811a7b14c4616154927d2df75207dd
solar05 <artem00298@gmail.com>
2021-06-14 22:41:53 +0300

    Add inn check

--- lib/dockcheck/inn.rb
+++ lib/dockcheck/inn.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class DockCheck::Inn
+  @@ten_digits_map = [2, 4, 10, 3, 5, 9, 4, 6, 8]
+  @@twelve_digits_first_map = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+  @@twelve_digits_second_map = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+
   def check(numbers)
     inn = prepare_inn(numbers)
     case inn.count
@@ -9,20 +13,39 @@ class DockCheck::Inn
     when 12
       twelve_digit_inn_valid?(inn)
     else
-      raise 'Incorrent inn numbers count!'
+      raise StandardError.new 'Incorrect inn numbers count!'
     end
   end
 
   private
   def prepare_inn(numbers)
-    numbers.split('')
+    numbers.split('').map { |n| n.to_i }
+  end
+
+  def control_sum_calc(inn, coefficients)
+    inn
+      .zip(coefficients)
+      .filter { |(a, b)| !b.nil? }
+      .reduce(0) { |acc, (a, b)| a * b + acc }
+  end
+
+  def sum_div(number)
+    number % 11 % 10
   end
 
   def ten_digit_inn_valid?(inn)
-    true
+    last_number = inn.last
+    control_sum = control_sum_calc(inn, @@ten_digits_map)
+    last_number == sum_div(control_sum)
   end
 
   def twelve_digit_inn_valid?(inn)
-    true
+    last_number = inn.last
+    penult_number = inn.fetch(10)
+
+    first_sum = control_sum_calc(inn, @@twelve_digits_first_map)
+    second_sum = control_sum_calc(inn, @@twelve_digits_second_map)
+
+    penult_number == sum_div(first_sum) && last_number == sum_div(second_sum)
   end
 end
---
+++ spec/dockcheck/inn_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe DockCheck::Inn do
+  let(:checker) { DockCheck.new(:inn) }
+  let(:ten_digit_valid_inn) { "7743013901" }
+  let(:ten_digit_invalid_inn) { "7743013902" }
+  let(:twelve_digit_valid_inn) { "732897853530" }
+  let(:twelve_digit_invalid_inn) { "732897853531" }
+
+  it "check inn correctly" do
+    expect(checker.check(ten_digit_valid_inn)).to be true
+    expect(checker.check(ten_digit_invalid_inn)).to be false
+
+    expect(checker.check(twelve_digit_valid_inn)).to be true
+    expect(checker.check(twelve_digit_invalid_inn)).to be false
+  end
+
+  it "throws exception when length invalid" do
+    expect{ checker.check("8800555") }.to raise_error 'Incorrect inn numbers count!'
+  end
+end
